5d41fdca36702258c3c62c10a409823c45efdd77
Roberto Palloni <rpdev20@gmail.com>
2021-12-01 21:28:26 +0100

    dynamodb

---
+++ aws_dynamodb.py
@@ -0,0 +1,29 @@
+import boto3 # sdk
+
+client = boto3.client('dynamodb',
+                      # keys for user with IAM permissions on dynamodb
+                      aws_access_key_id='iam_key',
+                      aws_secret_access_key='iam_secret_key',
+                      region_name='selected_region')
+
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table('Users')
+
+# GetItem
+response = table.get_item(
+    Key={'id': '1'}
+)
+print(response['Item'])
+
+# Scan (get all)
+response = table.scan()
+print(response['Items'])
+
+# PutItem
+table.put_item(
+    Item={
+        'id': '3',
+        'firstname': 'Paul',
+        'lastname': 'Kerry'
+    }
+)
